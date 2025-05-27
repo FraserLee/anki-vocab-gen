60d3de9b4b2cf39a2e734ed6c9c142701fec398a
Fraser <tomatoesneverlie@gmail.com>
2025-05-27 17:01:15 -0400

    add pyqt

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
+
 def main():
-    print("Hello from anki-vocab-gen!")
+    app = QApplication(sys.argv)
+    label = QLabel("Hello from anki-vocab-gen!")
+    label.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
