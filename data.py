c442ceb22bb9b65a850e0e6474c42361198af905
Fraser <tomatoesneverlie@gmail.com>
2025-06-11 14:05:43 -0400

    add way to generate image paths.

--- data.py
+++ data.py
@@ -1,10 +1,23 @@
 import os
+import re
 import nltk
+
+data_dir = os.path.expanduser('~/.anki_card_gen')
+images_dir = os.path.join(data_dir, 'images')
+
 def init() -> None:
-    data_dir = os.path.expanduser('~/.anki_card_gen')
     os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(images_dir, exist_ok=True)
     nltk.data.path.insert(0, data_dir)
 
     nltk.download('wordnet', download_dir=data_dir)
     nltk.download('omw-1.4', download_dir=data_dir)
 
+def get_new_image_path(word: str, extension: str) -> str:
+    sanitised_word = re.sub(r'[<>:"/\\|?*\s]', '_', word)
+    i = 0
+    potential_path = os.path.join(images_dir, f"{sanitised_word}.{extension}")
+    while os.path.exists(potential_path):
+        i += 1
+        potential_path = os.path.join(images_dir, f"{sanitised_word}_{i}.{extension}")
+    return potential_path
